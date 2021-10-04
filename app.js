62a90f95d658b1dbc5d65062d95eb23bd485a295
Karan Sethia <karansethia24@gmail.com>
2021-10-05 01:30:55 +0530

    Fixed search functionality

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ function randomGenerator(min, max) {
   
   function getName(event){
       if(event.keyCode === 13){
+        document.getElementById('myQuote').style.display = 'none';
+        document.getElementById('text-box').style.top = '20%';
+        document.getElementById('search-box').style.animation = 'upward-fade 3s ease-in-out forwards';
     getBook();}
 }
 //   console.log(book);
@@ -38,36 +41,36 @@ function randomGenerator(min, max) {
       fetch('librarybooks.json')
       .then(res => res.json())
       .then((Books) => {
-          Books.forEach(stack => {
-              switch(stack["Name"] === book){
-                case true:
-                    console.log("success");
+          function searchBook(bookname){
+                return Books.filter(
+                    function(Books){
+                        return Books["Name"] == bookname;
+                    }
+                );
+          }
+          var found = searchBook(book);
+          console.log(found);
+          if(found.length == 1){
+            console.log("success");
             console.log(book);
             document.getElementById('search-result').innerHTML= `<div id="result">
             <div id="bookInfo">
             <ul>
-                <li><h3>NAME: ${stack["Name"]}</h3></li>
-                <li><h3>GENRE: ${stack["Genre"]}</h3></li>
-                <li><h3>RATING: ${stack["Rating"]}</h3></li>
+                <li><h3>NAME: ${found[0]["Name"]} </h3></li>
+                <li><h3>GENRE: ${found[0]["Genre"]}</h3></li>
+                <li><h3>RATING: ${found[0]["Rating"]}</h3></li>
             </ul>
         </div>
         <div id="bookImg">
-        <img src="${stack["Book Cover"]}" height="150px" width="100px">
+        <img src="${found[0]["Book Cover"]}" height="150px" width="100px">
         </div>
         </div>`
-        break;
-        case false:
-        return n = 0;   
-              }
-          });
-          console.log(n);
-        //   if(n === 0){
-        //       console.log("not found");
-        //       document.getElementById('search-result').innerHTML=`<div id="result" class="result">
-        //       <h2>Sorry</h2>
-        //       <p>The Book you've been looking for is not found</p>
-        //       </div>`;
-        //   };
+          }else{
+            document.getElementById('search-result').innerHTML= `<div id="result" class="result">
+            <h2>Sorry</h2>
+            <p>The Book was not found</p>
+            </div>`;
+          }
       })
   }
   
